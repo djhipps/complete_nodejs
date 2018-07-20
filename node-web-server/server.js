db47b5d35b4b5b5987fd5f3e55a97a5a698b44df
Damian Hippisley <d.j.hippisley@gmail.com>
2018-07-20 17:01:08 +0100

    continuing adventures

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,21 +1,49 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+   var now = new Date().toString();
+   var log = `${now}: ${req.method} ${req.url}`
+   console.log(log);
+   fs.appendFile('server.log', log + '\n', (err) => {
+      if(err){
+          console.log('Unable to append to server.log');
+      }
+   });
+   next();
+});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+ return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello express!</h1>');
-    res.send({
-      name:"Damian",
-      likes: [
-        'Emotions',
-        'JS'
-      ]
+    res.render('home.hbs', {
+      pageTitle:'Home page',
+      welcomeMessage: 'Welcome to some website'
     });
+
 });
 
 app.get('/about', (req, res) => {
-  res.send('<p>About Page</p>');
-
+    res.render('about.hbs', {
+         pageTitle:'About page',
+    });
 });
 
 app.get('/bad', (req, res) => {
@@ -24,4 +52,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+   console.log(`Server is up on port ${port}`);
+});
